c3a5b5d80a9618dbbc00e7531a5f25a92388d446
kouame konan abraham <abrahamkonany@gmail.com>
2023-05-08 16:41:25 +0200

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -5,3 +5,8 @@ exports.UserAccount = (req,res, next)=>{
         msg:"message valide !!!"
     })
 }
+exports.Userlog = (req, res, next)=>{
+    res.status(200).json({
+        msg: "log valide !!"
+    })
+}
