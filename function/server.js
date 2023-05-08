12c0f6c95342ec95f8cdd743534b3e7e4a6a73ef
kouame konan abraham <abrahamkonany@gmail.com>
2023-05-08 13:56:27 +0200

    ok

--- function/server.js
+++ function/server.js
@@ -3,9 +3,11 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
-router.get('/',(req, res)=> res.send("hello my serverless avaibale!!"))
+router.get('/',(req, res)=> {
+    res.send("hello my serverless avaibale!!")
+})
 
-app.use('/.netlify/function/api', router )
+app.use('/.netlify/functions/server', router )
 
 
 
