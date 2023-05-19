0ecda34b3e3e41764907c6bddac37b6ec55e7918
kouame konan abraham <abrahamkonany@gmail.com>
2023-05-19 17:13:29 +0000

    ok

--- controller/pret.js
+++ controller/pret.js
@@ -2,5 +2,10 @@ const User = require('../model/user');
 const Solde = require('../model/solde');
 
 exports.preCompte = (req,res,next)=>{
-    res.send("ok")
+    User.findOne({_id:req.auth.userId})
+    .then((data)=> {
+        res.send("ok")
+    })
+    .catch((error)=> res.status(404).json({error.message}));
+    
 }
